c7af527f287004ba9d184573602b589213b9120d
Anders Arnholm <anders@arnholm.se>
2020-06-30 07:19:34 +0200

    Get the TreeTests running

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+module Main where
+
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.HUnit.Base
+import TreeTest
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain (treeTests)
